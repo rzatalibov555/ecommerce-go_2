7c311f08a4897ad39ea1f15025909a2daaff1ac6
rzatalibov555 <sethubpro@gmail.com>
2025-06-24 21:44:28 +0400

    Postman requests

--- product/api/urls.py
+++ product/api/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from product.api.views import product_list_create_view, product_detail_view
+from product.api.views import product_list_create_view, product_detail_view, index_view, product_create_view, product_delete_view, product_update_put_view, product_update_patch_view
 
 app_name = "product/api"
 
@@ -7,23 +7,17 @@ app_name = "product/api"
 urlpatterns = [
     path("products/", product_list_create_view, name="products_list_create"), # "GET", "POST"
     path("products/<int:id>", product_detail_view, name="products_detail"),   # "GET", "PUT", "PATCH", "DELETE"
-
 ]
 
 
 
 
-
-
-
-
-
 # urlpatterns = [
-#     path("index/", views.index_view, name="index"),
-#     path("detail/<id>", views.product_detail_view, name="detail"),
-#     path("delete/<id>", views.product_delete_view, name="delete"),
-#     path("create/", views.product_create_view, name="create"),
+#     path("index/", index_view, name="index"),
+#     path("detail/<id>", product_detail_view, name="detail"),
+#     path("delete/<id>", product_delete_view, name="delete"),
+#     path("create/", product_create_view, name="create"),
 
-#     path("update_put/<id>", views.product_update_put_view, name="update_put"),
-#     path("update_patch/<id>", views.product_update_patch_view, name="update_patch"),
+#     path("update_put/<id>", product_update_put_view, name="update_put"),
+#     path("update_patch/<id>", product_update_patch_view, name="update_patch"),
 # ]
--- product/api/views.py
+++ product/api/views.py
@@ -105,101 +105,101 @@ def product_detail_view(request, id):
 
 # GET, POST, PUT, PATCH, DELETE
 # ------------------------------------------------------------------------------
-# @api_view(["GET"])
-# def index_view(request):
-#     # product = Product.objects.all()
+@api_view(["GET"])
+def index_view(request):
+    # product = Product.objects.all()
 
-#     product = Product.objects.annotate(
-#         discount_price=Coalesce('discount', Value(0), output_field=FloatField()),
-#         coupon_price=Coalesce('coupon', Value(0), output_field=FloatField()),
-#         tax=Coalesce('tax_price', Value(0), output_field=FloatField()),
-#         ).annotate(
-#         total_price=Round(
-#             ExpressionWrapper(
-#                 (F('price') * (1 - F('discount_price') / 100) - F('coupon_price') + F('tax')),
-#                 output_field=DecimalField(max_digits=10, decimal_places=2)
-#             ),
-#             precision=2
-#         )
-#     )
+    product = Product.objects.annotate(
+        discount_price=Coalesce('discount', Value(0), output_field=FloatField()),
+        coupon_price=Coalesce('coupon', Value(0), output_field=FloatField()),
+        tax=Coalesce('tax_price', Value(0), output_field=FloatField()),
+        ).annotate(
+        total_price=Round(
+            ExpressionWrapper(
+                (F('price') * (1 - F('discount_price') / 100) - F('coupon_price') + F('tax')),
+                output_field=DecimalField(max_digits=10, decimal_places=2)
+            ),
+            precision=2
+        )
+    )
 
-#     serializer =  ProductListSerializer(product, many=True)
-#     print(serializer.data)
-#     return Response(serializer.data)
+    serializer =  ProductListSerializer(product, many=True)
+    print(serializer.data)
+    return Response(serializer.data)
 
 
-# @api_view(["GET"])
-# def product_detail_view(request, id):
+@api_view(["GET"])
+def product_detail_view(request, id):
 
-#     # product = Product.objects.all()
+    # product = Product.objects.all()
     
-#     product = Product.objects.annotate(
-#         discount_price=Coalesce('discount', Value(0), output_field=FloatField()),
-#         coupon_price=Coalesce('coupon', Value(0), output_field=FloatField()),
-#         tax=Coalesce('tax_price', Value(0), output_field=FloatField()),
-#         ).annotate(
-#         total_price=Round(
-#             ExpressionWrapper(
-#                 (F('price') * (1 - F('discount_price') / 100) - F('coupon_price') + F('tax')),
-#                 output_field=DecimalField(max_digits=10, decimal_places=2)
-#             ),
-#             precision=2
-#         )
-#     )
+    product = Product.objects.annotate(
+        discount_price=Coalesce('discount', Value(0), output_field=FloatField()),
+        coupon_price=Coalesce('coupon', Value(0), output_field=FloatField()),
+        tax=Coalesce('tax_price', Value(0), output_field=FloatField()),
+        ).annotate(
+        total_price=Round(
+            ExpressionWrapper(
+                (F('price') * (1 - F('discount_price') / 100) - F('coupon_price') + F('tax')),
+                output_field=DecimalField(max_digits=10, decimal_places=2)
+            ),
+            precision=2
+        )
+    )
 
-#     product = product.get(id=id)
+    product = product.get(id=id)
     
-#     serializer = ProductListSerializer(product, many=False)
+    serializer = ProductListSerializer(product, many=False)
 
-#     return Response(serializer.data)
+    return Response(serializer.data)
 
 
-# @api_view(["POST"])
-# def product_create_view(request):
-#     serializer = ProductCreateSerializer(data=request.data)
-#     serializer.is_valid(raise_exception=True)
-#     serializer.save()
+@api_view(["POST"])
+def product_create_view(request):
+    serializer = ProductCreateSerializer(data=request.data)
+    serializer.is_valid(raise_exception=True)
+    serializer.save()
 
-#     return Response(serializer.data)
+    return Response(serializer.data)
 
-# @api_view(["DELETE"])
-# def product_delete_view(request, id):
-#     product = Product.objects.annotate(
-#         discount_price=Coalesce('discount', Value(0), output_field=FloatField()),
-#         coupon_price=Coalesce('coupon', Value(0), output_field=FloatField()),
-#         tax=Coalesce('tax_price', Value(0), output_field=FloatField()),
-#         ).annotate(
-#         total_price=Round(
-#             ExpressionWrapper(
-#                 (F('price') * (1 - F('discount_price') / 100) - F('coupon_price') + F('tax')),
-#                 output_field=DecimalField(max_digits=10, decimal_places=2)
-#             ),
-#             precision=2
-#         )
-#     )
+@api_view(["DELETE"])
+def product_delete_view(request, id):
+    product = Product.objects.annotate(
+        discount_price=Coalesce('discount', Value(0), output_field=FloatField()),
+        coupon_price=Coalesce('coupon', Value(0), output_field=FloatField()),
+        tax=Coalesce('tax_price', Value(0), output_field=FloatField()),
+        ).annotate(
+        total_price=Round(
+            ExpressionWrapper(
+                (F('price') * (1 - F('discount_price') / 100) - F('coupon_price') + F('tax')),
+                output_field=DecimalField(max_digits=10, decimal_places=2)
+            ),
+            precision=2
+        )
+    )
 
-#     product = product.get(id=id)
+    product = product.get(id=id)
 
-#     product.delete()
-#     return Response({"Messege":"Product successfully deleted!"})
+    product.delete()
+    return Response({"Messege":"Product successfully deleted!"})
 
 # ------------------------------------------------------------------------------
 
-# @api_view(["PUT"]) # Butun sutunlari update etmek lazimdir. // partial=True lazim deyil
-# def product_update_put_view(request, id):
-#     product = get_object_or_404(Product, id=id)
-#     serializer = ProductUpdateSerializer(instance=product, data=request.data)
-#     serializer.is_valid(raise_exception=True)
-#     serializer.save()
-#     return Response(serializer.data)
+@api_view(["PUT"]) # Butun sutunlari update etmek lazimdir. // partial=True lazim deyil
+def product_update_put_view(request, id):
+    product = get_object_or_404(Product, id=id)
+    serializer = ProductUpdateSerializer(instance=product, data=request.data)
+    serializer.is_valid(raise_exception=True)
+    serializer.save()
+    return Response(serializer.data)
 
-# @api_view(["PATCH"]) # Lazim olan sutunlari update etmek olar. // partial=True olmalidir
-# def product_update_patch_view(request, id):
-#     product = get_object_or_404(Product, id=id)
-#     serializer = ProductUpdateSerializer(instance=product, data=request.data, partial=True)
-#     serializer.is_valid(raise_exception=True)
-#     serializer.save()
-#     return Response(serializer.data)
+@api_view(["PATCH"]) # Lazim olan sutunlari update etmek olar. // partial=True olmalidir
+def product_update_patch_view(request, id):
+    product = get_object_or_404(Product, id=id)
+    serializer = ProductUpdateSerializer(instance=product, data=request.data, partial=True)
+    serializer.is_valid(raise_exception=True)
+    serializer.save()
+    return Response(serializer.data)
 # ------------------------------------------------------------------------------
 
    
